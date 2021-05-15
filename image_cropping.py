0a6ec696095f69571e74d9d99de5b96139fa34d0
Gary Sweeney <59615188+garysweeney@users.noreply.github.com>
2021-05-15 13:07:19 -0400

    Update image_cropping.py

--- image_cropping.py
+++ image_cropping.py
@@ -5,7 +5,14 @@ from PIL import Image
 from math import ceil
 
 #Open image (I used a .tif file):
-img = plt.imread ("img_name.tif")
+img = plt.imread ("LowDoseCT.tif")
+
+#Display original image
+original_image = plt.figure()
+original_image.suptitle("Low Dose CT Scan")
+original_image = plt.axis('off')
+original_image = plt.imshow(img,cmap='gray')
+original_image = plt.show()
 
 #Find size of the image:
 x_size, y_size = img.shape
@@ -16,7 +23,7 @@ max_x = x_size - min_x
 min_y = int (y_size/3)
 max_y = y_size - min_x
 
-cropped_img = img [x_min : x_max, y_min : y_max]
+cropped_img = img [min_x : max_x , min_y : max_y]
 
 #display cropped image:
 plt.imshow (cropped_img)
